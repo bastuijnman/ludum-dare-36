67db9b7a61b64ab11345efef546fe69c54099ea4
Bas Tuijnman <bas.tuijnman@24i.com>
2016-08-27 21:45:31 +0200

    Add methods to buy stuff

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,9 +3,20 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	private int money = 500;
 
+	public int GetMoney () {
+		return money;
+	}
+
+	public bool Buy (int price) {
+		if (price > money) {
+			return false;
+		}
+
+		money -= price;
+
+		return true;
 	}
 
 	// Update is called once per frame
