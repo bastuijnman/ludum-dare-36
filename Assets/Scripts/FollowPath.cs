669148bd1893c536a92c70133ff59c76e52493d5
Bas Tuijnman <bas.tuijnman@24i.com>
2016-08-27 16:51:40 +0200

    Own pathing system

---
+++ Assets/Scripts/FollowPath.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FollowPath : MonoBehaviour {
+
+	public GameObject path;
+
+	public Transform[] waypoints;
+
+	int currentWaypoint = 0;
+
+	float speed = 5;
+
+	// Use this for initialization
+	void Start () {
+		List<Transform> w = new List<Transform> ();
+
+		foreach (Transform waypoint in path.transform) {
+			w.Add (waypoint);
+		}
+
+		waypoints = w.ToArray ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentWaypoint != (waypoints.Length - 1)) {
+			float step = speed * Time.deltaTime;
+			transform.position = Vector3.MoveTowards (transform.position, waypoints [currentWaypoint + 1].position, step);
+		}
+			
+	}
+
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.tag == "waypoint") {
+			currentWaypoint++;
+		}
+	}
+		
+}
